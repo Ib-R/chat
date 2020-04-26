37937f1647b34a18db11b5f41b702aee51797371
IBR <business1232@gmail.com>
2020-04-26 08:57:38 +0200

    Updates

---
+++ precache-manifest.6d08c236caf70c8c06e3caa564cdff44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f62f0bbdeef03da19ffb2a4981ccfb",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "2e477245b90942d43f93",
+    "url": "/chat/static/css/main.0ac04aaf.chunk.css"
+  },
+  {
+    "revision": "ef4fc749e6dcc4a766e6",
+    "url": "/chat/static/js/2.bc7d9ced.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chat/static/js/2.bc7d9ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa4925658b4b35644ca",
+    "url": "/chat/static/js/3.d33abb37.chunk.js"
+  },
+  {
+    "revision": "2e477245b90942d43f93",
+    "url": "/chat/static/js/main.99d50cfd.chunk.js"
+  },
+  {
+    "revision": "a604e7b878f6ad549682",
+    "url": "/chat/static/js/runtime-main.719ae32e.js"
+  }
+]);
